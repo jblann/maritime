6fefbce193115536aceef1d06ea93553bc03da15
Ryan Kohl <ryanckohl@gmail.com>
2013-02-17 22:53:47 -0500

    more initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject maritime "0.1"
+  :description "Analytic project based on maritime shipping dataset"
+  :url "http://github.com/ryankohl/maritime"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/data.xml "0.0.7"]])
---
+++ src/maritime/raw-parse.clj
@@ -0,0 +1,38 @@
+(ns maritime.raw-parse
+  (:use [clojure.data.xml])
+  (:require [clojure.java.io :as io]))
+
+(defn dive [keyword element]
+  (first (filter #(= keyword (:tag %)) (:content element))))
+(defn grab [keyword element]
+  (->> element (dive keyword) :content first))
+
+(defn process-feature [feature]
+  (let [vessel-data (dive :VesselData feature)
+        track (->> vessel-data (dive :VesselTrack) (dive :VesselMovingObjectStatus))]
+    {:id (-> vessel-data :attrs :vesselSourceID)
+     :name (->> vessel-data (grab :sailedAsName))
+     :mmsi (->> vessel-data (grab :sailedAsMmsi))
+     :destPort (->> vessel-data (grab :destPort))
+     :eta (->> vessel-data (grab :eta))
+     :maxDraft (->> vessel-data (grab :maxDraft))
+     :type (->> vessel-data (grab :vesselTypeCode))
+     :length (->> vessel-data (grab :sailedAsLength))
+     :timestamp (->> track (grab :timestamp))
+     :coords (->> track (dive :location) (dive :Point) (grab :coordinates))
+     :speed (->> track (grab :speed))
+     :trueHeading (->> track (grab :trueHeading))
+     :navStatus (->> track (grab :navigationStatus))
+     :courseOverGround (->> track (grab :courseoverGround))
+     :receivingStation (->> track (grab :receivingStation))
+     :rateOfTurn (->> track (grab :rateofTurn))
+     :receiverClass (->> track (grab :receiverClass))
+     :messageType (->> track (grab :aisMessageType))}))
+
+(defn process-file [filename]
+  (let [data (-> filename io/input-stream parse)
+        features (->> data 
+                      :content 
+                      (filter #(= :featureMember (:tag %))) 
+                      (map process-feature))]
+    {:data features}))
