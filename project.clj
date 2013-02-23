330cc64ed415a8bf9ff0a78c23402dad299cac67
Ryan Kohl <ryanckohl@gmail.com>
2013-02-23 17:42:00 -0500

    added storm topology for parsing

--- project.clj
+++ project.clj
@@ -3,5 +3,13 @@
   :url "http://github.com/ryankohl/maritime"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src"]
+  :java-source-paths ["src/jvm"]
+  :javac-options {:debug "true"}
+  :resources-paths ["multilang"]
+  :aot :all
+  :repositories {}
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [storm "0.8.2"]
+                 [commons-collections/commons-collections "3.2.1"]
                  [org.clojure/data.xml "0.0.7"]])
